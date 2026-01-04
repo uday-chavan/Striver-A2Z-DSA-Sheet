d689aad4e8bd1523383df06e3881113cf98768e5
uday-chavan <chavanuday407@gmail.com>
2026-01-04 12:11:23 +0530

    Add C++ basics problems: input-output, if-else, switch-case, arrays, loops

---
+++ 1. input-output.cpp
@@ -0,0 +1,53 @@
+// #include <iostream>
+// int main()
+// {
+//     std::cout << "Hello, World!" << "\n";
+//     std::cout << "Hello again" << "\n";
+// }
+
+// #include <iostream>
+// int main()
+// {
+//     std::cout << "Hey, Uday!" << std::endl;
+//     std::cout << "Hey, Uday again";
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << "hey uday" << endl;
+//     cout << "hey uday again";
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cin >> x;
+//     cout << "Value of x: " << x;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int x, y;
+//     cin >> x >> y;
+//     cout << "Value of x: " << x << " and y: " << y;
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << "Value of x: " << x << " and y: " << y;
+    return 0;
+}
