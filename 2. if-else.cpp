40164af6613a8248eebb87d4e7e7146127808ed6
uday-chavan <chavanuday407@gmail.com>
2026-01-04 11:49:07 +0530

    Add initial DSA exercises

---
+++ 2. if-else.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks = 45;
+
+    if (marks < 25)
+    {
+        cout << "F" << endl;
+    }
+    else if (marks >= 25 && marks <= 45)
+    {
+        cout << "E" << endl;
+    }
+    else if (marks > 45 && marks <= 50)
+    {
+        cout << "D" << endl;
+    }
+    else if (marks > 50 && marks <= 60)
+    {
+        cout << "C" << endl;
+    }
+    else if (marks > 60 && marks <= 80)
+    {
+        cout << "B" << endl;
+    }
+    else if (marks > 80 && marks <= 100)
+    {
+        cout << "A" << endl;
+    }
+    else
+    {
+        cout << "Invalid!" << endl;
+    }
+    return 0;
+}
