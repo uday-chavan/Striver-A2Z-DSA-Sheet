e695a675cb113606ea343acc3cd700838911f6b9
uday-chavan <chavanuday407@gmail.com>
2026-01-06 16:03:17 +0530

    Patterns Done

--- Step-01-Basics/Pattern-problems/patterns.cpp
+++ Step-01-Basics/Pattern-problems/patterns.cpp
@@ -419,9 +419,43 @@ void print20(int n)
             spaces += 2;
     }
 }
+
+void print21(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
+            {
+                cout << "*";
+            }
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print22(int n)
+{
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
+            int top = i;
+            int left = j;
+            int right = (2 * n - 2) - j;
+            int down = (2 * n - 2) - i;
+            cout << (n - min(min(top, down), min(left, right)));
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int n;
     cin >> n;
-    print20(n);
+    print22(n);
 }
