71625a9e3a8688f92079cde2c84d728deef58c74
uday-chavan <chavanuday407@gmail.com>
2026-01-07 21:03:02 +0530

    Added Palinrome Problem Solution

--- Step-01-Basics/Basic-Maths/maths.cpp
+++ Step-01-Basics/Basic-Maths/maths.cpp
@@ -34,15 +34,17 @@ int reverseNumber(int n)
 
 bool checkPalindrome(int x)
 {
-    int copy = x;
     if (x < 0)
-    {
         return false;
-    }
+
+    int copy = x;
     int rev = 0;
+
     while (x != 0)
     {
         int digit = x % 10;
+        if (rev > INT_MAX / 10 || rev < INT_MIN / 10)
+            return false;
         rev = rev * 10 + digit;
         x = x / 10;
     }
