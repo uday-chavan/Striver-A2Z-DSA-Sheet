1db0283a8921b25a85244d58c9c41cf6dabf7a82
uday-chavan <chavanuday407@gmail.com>
2026-01-10 00:52:08 +0530

    Solved More Recursion Problems

--- Step-01-Basics/Recursion/Basic Recursion Problems/Recursion.cpp
+++ Step-01-Basics/Recursion/Basic Recursion Problems/Recursion.cpp
@@ -43,11 +43,81 @@ void n2Onewithoutminusone(int i, int n)
     cout << i << " ";
 }
 
+void sumOfFirstNparameterized(int i, int sum)
+{
+    if (i < 1)
+    {
+        cout << (sum) << " ";
+        return;
+    }
+    sumOfFirstNparameterized(i - 1, sum + i);
+}
+
+int sumOfFirstNfunctional(int n)
+{
+    if (n == 0)
+        return 0;
+
+    return n + sumOfFirstNfunctional(n - 1);
+}
+
+int factorialOfN(int n)
+{
+    if (n == 0)
+        return 1;
+    return n * factorialOfN(n - 1);
+}
+
+void reverseArray(int i, int arr[], int n)
+{
+    if (i >= n / 2)
+        return;
+    swap(arr[i], arr[n - i - 1]);
+    reverseArray(i + 1, arr, n);
+}
+
+bool checkstringIsPalindromeOrNot(int i, const string &s)
+{
+    if (i >= s.size() / 2)
+        return true;
+    if (s[i] != s[s.size() - i - 1])
+        return false;
+    return checkstringIsPalindromeOrNot(i + 1, s);
+}
+
+bool isPalindrome(string s)
+{
+    string cleaned = "";
+
+    for (char c : s)
+    {
+        if (isalnum(c))
+            cleaned += tolower(c);
+    }
+    return checkstringIsPalindromeOrNot(0, cleaned);
+}
+
 int main()
 {
-    int n;
-    cin >> n;
-
-    n2Onewithoutminusone(1, n);
-    return 0;
 }
+
+// stringIsPalindromeOrNot //
+
+// string s = "A man, a plan, a canal: Panama";
+// cout << isPalindrome(s);
+// return 0;
+
+// ReverseArray//
+
+// int n;
+// cin >> n;
+// int arr[n];
+// for (int i = 0; i < n; i++)
+//     cin >> arr[i];
+
+// reverseArray(0, arr, n);
+
+// for (int i = 0; i < n; i++)
+//     cout << arr[i] << " ";
+
+// return 0;
