5823fec60a29caa4e75fc6ba02c42fe0d400b952
uday-chavan <chavanuday407@gmail.com>
2026-01-11 01:03:33 +0530

    Hashing Concept and Use

--- Step-01-Basics/Hashing/hashing.cpp
+++ Step-01-Basics/Hashing/hashing.cpp
@@ -1,31 +1,61 @@
 #include <iostream>
 using namespace std;
 
+// number hashing //
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+
+//     int hash[13] = {0};
+//     for (int i = 0; i < n; i++)
+//     {
+//         hash[arr[i]] += 1;
+//     }
+
+//     int q;
+//     cin >> q;
+//     while (q--)
+//     {
+//         int number;
+//         cin >> number;
+
+//         cout << hash[number] << endl;
+//     }
+//     return 0;
+// }
+
+// character hashing //
+
 int main()
 {
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
+    string s;
+    cin >> s;
 
-    int hash[13] = {0};
-    for (int i = 0; i < n; i++)
+    // pre compute
+    int hash[26] = {0};
+    for (int i = 0; i < s.size(); i++)
     {
-        hash[arr[i]] += 1;
+        hash[s[i] - 'a']++;
     }
-
     int q;
     cin >> q;
     while (q--)
     {
-        int number;
-        cin >> number;
-
-        cout << hash[number] << endl;
+        char c;
+        cin >> c;
+        // fetch
+        cout << hash[c - 'a'] << endl;
     }
     return 0;
 }
+// if lower case characters = s[i] - 'a' & c - 'a'
+// if upper case characters = s[i] - 'A' & c - 'a'
+// if all characters, i.e., hash[256] = only s[i] & c[i]
+
 // array can go upto 10^6 in main or 10^7 globally
