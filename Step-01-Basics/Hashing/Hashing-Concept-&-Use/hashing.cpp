a6b6ff459e16598771c3db96aed0fcea754e93da
uday-chavan <chavanuday407@gmail.com>
2026-01-11 18:45:42 +0530

    Hashing

--- Step-01-Basics/Hashing/Hashing-Concept-&-Use/hashing.cpp
+++ Step-01-Basics/Hashing/Hashing-Concept-&-Use/hashing.cpp
@@ -2,61 +2,59 @@
 using namespace std;
 
 // number hashing //
-// int main()
-// {
-//     int n;
-//     cin >> n;
-//     int arr[n];
-//     for (int i = 0; i < n; i++)
-//     {
-//         cin >> arr[i];
-//     }
+void numberHashing()
+{
+    int n;
+    cin >> n;
 
-//     int hash[13] = {0};
-//     for (int i = 0; i < n; i++)
-//     {
-//         hash[arr[i]] += 1;
-//     }
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
 
-//     int q;
-//     cin >> q;
-//     while (q--)
-//     {
-//         int number;
-//         cin >> number;
+    int hash[13] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        hash[arr[i]] += 1;
+    }
 
-//         cout << hash[number] << endl;
-//     }
-//     return 0;
-// }
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int number;
+        cin >> number;
+
+        cout << hash[number] << endl;
+    }
+}
 
 // character hashing //
-// int main()
-// {
-//     string s;
-//     cin >> s;
+void characterHashing()
+{
+    string s;
+    cin >> s;
 
-//     // pre compute
-//     int hash[26] = {0};
-//     for (int i = 0; i < s.size(); i++)
-//     {
-//         hash[s[i] - 'a']++;
-//     }
-//     int q;
-//     cin >> q;
-//     while (q--)
-//     {
-//         char c;
-//         cin >> c;
-//         // fetch
-//         cout << hash[c - 'a'] << endl;
-//     }
-//     return 0;
-// }
+    // pre compute
+    int hash[26] = {0};
+    for (int i = 0; i < s.size(); i++)
+    {
+        hash[s[i] - 'a']++;
+    }
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        char c;
+        cin >> c;
+        // fetch
+        cout << hash[c - 'a'] << endl;
+    }
+}
 
 // map - ordered & unordered map //
-
-int main()
+void orderedUnorderedMap()
 {
     int n;
     cin >> n;
@@ -73,10 +71,10 @@ int main()
     }
 
     // iterate in the map (if want to see how elements are stored in map)
-    // for (auto it : mpp)
-    // {
-    //     cout << it.first << "->" << it.second << endl;
-    // }
+    for (auto it : mpp)
+    {
+        cout << it.first << "->" << it.second << endl;
+    }
 
     int q;
     cin >> q;
@@ -89,17 +87,23 @@ int main()
     }
 }
 
+int main()
+{
+    orderedUnorderedMap();
+    return 0;
+}
+
 // if lower case characters = s[i] - 'a' & c - 'a'
 // if upper case characters = s[i] - 'A' & c - 'a'
 // if all characters, i.e., hash[256] = only s[i] & c[i]
 
 // array hashing can go upto 10^6 in main or 10^7 globally
 
-// Anything storing or fetching in map take log N in all best, svg, worst time complexity.
-// always give first preference to unordered map because the worst case happens very rarely due to internal collisionsb;if such happens then you should switch to map. (unordered_map[])
+// Anything storing or fetching in map take log N in all best, avg, worst time complexity.
+// always give first preference to unordered map because the worst case happens very rarely due to internal collisions like if there are all value ending with 8, all will store there values in 8 and remaning will be empty;if such happens then you should switch to map. (unordered_map[])
 
 // Hashing //
 
-// Division method
+// Division method => arr[i] % 10 , and store the value in hash and retrieve the value from that hash
 // Folding method
 // Mid Square method
---
+++ Step-01-Basics/Hashing/Hashing-Problems/hashing-problems.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void countFrequencyOfEachElement(int arr[], int n)
+{
+    vector<bool> visited(n, false);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (visited[i] == true)
+            continue;
+
+        int count = 1;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                visited[j] = true;
+                count++;
+            }
+        }
+        cout << arr[i] << " " << count << endl;
+    }
+}
+
+void countFreqOptimal(int arr[], int n)
+{
+    unordered_map<int, int> map;
+
+    for (int i = 0; i < n; i++)
+    {
+        map[arr[i]]++;
+    }
+    for (auto &x : map)
+    {
+        cout << x.first << " " << x.second << endl;
+    }
+}
+
+void freqOfMostFreq(vector<int> &nums, int k)
+{
+    sort(nums.begin(), nums.end());
+
+    long long sum = 0;
+    int left = 0;
+    int ans = 1;
+
+    for (int right = 0; right < nums.size(); right++)
+    {
+        sum += nums[right];
+
+        while ((long long)nums[right] * (right - left + 1) - sum > k)
+        {
+            sum -= nums[left];
+            left++;
+        }
+
+        ans = max(ans, right - left + 1);
+    }
+
+    cout << "Maximum frequency = " << ans << endl;
+}
+
+int main()
+{
+    int arr[] = {10, 23, 45, 43, 34, 23};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    // because, sizeof(arr) = 6 * 4 = 24
+    // therefore, n = 24/4 = 6
+
+    // for FreqOfMostFreq
+    vector<int> nums(arr, arr + n); // array -> vector
+    int k = 5;                      // define k
+
+    freqOfMostFreq(nums, k); // function call
+    return 0;
+}
