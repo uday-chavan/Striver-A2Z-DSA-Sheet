dbc0a29e621212c97a6ef9a38106151b332ecddb
uday-chavan <chavanuday407@gmail.com>
2026-01-12 16:38:41 +0530

    Selection Sort-Bubble Sort

--- Step-02-Sorting Techniques/Sorting-I/Selection-Bubble-Insertion.cpp
+++ Step-02-Sorting Techniques/Sorting-I/Selection-Bubble-Insertion.cpp
@@ -22,6 +22,29 @@ void selection_sort(int arr[], int n)
     }
 }
 
+void bubble_sort(int arr[], int n)
+{
+    for (int i = n - 1; i >= 0; i--) // because last is n-1 in arrays
+    {
+        int didSwap = 0;
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                didSwap = 1;
+            }
+        }
+        if (didSwap == 0)
+        {
+            break;
+        }
+        cout << "runs\n"; // to check how many times it ran.
+    }
+}
+
 int main()
 {
     int n;
@@ -29,7 +52,7 @@ int main()
     int arr[n];
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    selection_sort(arr, n);
+    bubble_sort(arr, n);
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
