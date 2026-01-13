fad762d3685e8a53c1f3f8748e1e4c440f6f14bd
uday-chavan <chavanuday407@gmail.com>
2026-01-13 12:19:57 +0530

    Merge Sort & Quick Sort

--- Step-02-Sorting Techniques/Merge-Quick.cpp
+++ Step-02-Sorting Techniques/Merge-Quick.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Merge sort //
+
 void merge(vector<int> &arr, int low, int mid, int high)
 {
     vector<int> temp;
@@ -50,12 +52,46 @@ void mergeSort(vector<int> &arr, int n)
     mS(arr, 0, n - 1);
 }
 
+// Quick sort //
+
+int partition(vector<int> &arr, int low, int high)
+{
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while (i < j)
+    {
+        while (arr[i] <= pivot && i <= high - 1)
+        {
+            i++;
+        }
+        while (arr[j] > pivot && j >= low + 1)
+        {
+            j--;
+        }
+        if (i < j)
+            swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void qs(vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pIndex = partition(arr, low, high);
+        qs(arr, pIndex + 1, high);
+        qs(arr, pIndex + 1, high);
+    }
+}
+
 int main()
 {
     vector<int> arr = {5, 2, 9, 1, 3};
     int n = arr.size();
 
-    mergeSort(arr, n);
+    qs(arr, 0, n - 1);
 
     for (int x : arr)
         cout << x << " ";
